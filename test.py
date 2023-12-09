a06affb77db23c2158e0920321eb9e9f4aa64c95
sai-krishna-eluru <34314057+sai-krishna-eluru@users.noreply.github.com>
2023-12-09 00:17:40 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,82 +1,82 @@
 import unittest
 from unittest.mock import patch
 from io import StringIO
-from game import Player, Enemy, ItemShop, Game, battle
+from your_game_file import Player, Enemy, ItemShop, Game, battle
 
 class TestRPGGame(unittest.TestCase):
+    
+    def setUp(self):
+        self.player = Player(name="TestPlayer", health=50, attack=15, defense=8, level=2)
+        self.enemy = Enemy(name="TestEnemy", health=30, attack=10, defense=5, experience=15)
+        self.item_shop = ItemShop()
+        self.game_instance = Game()
 
     def test_player_attack_enemy(self):
-        player = Player(name="Player1", attack=15, defense=5)
-        enemy = Enemy(name="Goblin", health=20, defense=3)
-        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
-            player.attack_enemy(enemy)
-        expected_output = "Player1 attacks Goblin for 12 damage.\n"
-        self.assertEqual(mock_stdout.getvalue(), expected_output)
-
-    def test_player_level_up(self):
-        player = Player(name="Player1", level=2, attack=20, defense=8, health=80)
-        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
-            player.level_up()
-        expected_output = "Player1 leveled up to level 3!\n"
-        expected_output += "Player1's Health: 100, Player1's Attack: 25, Player1's Defense: 10\n"
-        self.assertEqual(mock_stdout.getvalue(), expected_output)
-
-    def test_player_use_health_potion(self):
-        player = Player(name="Player1", health=50, inventory=["Health Potion"])
-        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
-            player.use_item("Health Potion")
-        expected_output = "Player1 used a Health Potion and gained 30 health.\n"
-        self.assertEqual(mock_stdout.getvalue(), expected_output)
-        self.assertEqual(player.health, 80)
-        self.assertNotIn("Health Potion", player.inventory)
+        with patch("sys.stdout", new_callable=StringIO) as mock_stdout:
+            self.player.attack_enemy(self.enemy)
+            output = mock_stdout.getvalue().strip()
+            self.assertIn("TestPlayer attacks TestEnemy", output)
 
     def test_enemy_attack_player(self):
-        player = Player(name="Player1", health=50, defense=5)
-        enemy = Enemy(name="Orc", attack=12)
-        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
-            enemy.attack_player(player)
-        expected_output = "Orc attacks Player1 for 7 damage.\n"
-        self.assertEqual(mock_stdout.getvalue(), expected_output)
-        self.assertEqual(player.health, 43)
+        with patch("sys.stdout", new_callable=StringIO) as mock_stdout:
+            self.enemy.attack_player(self.player)
+            output = mock_stdout.getvalue().strip()
+            self.assertIn("TestEnemy attacks TestPlayer", output)
 
-    def test_item_shop_display_items(self):
-        item_shop = ItemShop()
-        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
-            item_shop.display_items()
-        expected_output = "Items available in the shop:\n- Health Potion\n- Attack Boost\n- Defense Boost\n"
-        self.assertEqual(mock_stdout.getvalue(), expected_output)
+    def test_level_up(self):
+        self.player.level_up()
+        self.assertEqual(self.player.level, 3)
+        self.assertEqual(self.player.attack, 20)
+        self.assertEqual(self.player.defense, 10)
+        self.assertEqual(self.player.health, 70)
 
-    def test_game_explore(self):
-        game = Game()
-        game.player = Player(name="Player1")
-        with patch('builtins.input', return_value="1"), patch('random.choice', return_value=Enemy()):
-            with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
-                game.explore()
-        self.assertIn("Player1 is exploring...", mock_stdout.getvalue())
+    def test_use_health_potion(self):
+        initial_health = self.player.health
+        self.player.inventory.append("Health Potion")
+        self.player.use_item("Health Potion")
+        self.assertEqual(self.player.health, initial_health + 30)
 
-    def test_game_visit_item_shop(self):
-        game = Game()
-        game.player = Player(name="Player1")
-        with patch('builtins.input', side_effect=["Health Potion", "exit"]):
-            with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
-                game.visit_item_shop()
-        expected_output = "Welcome to the Item Shop!\nItems available in the shop:\n- Health Potion\n- Attack Boost\n- Defense Boost\n"
-        expected_output += "Player1 bought Health Potion.\n"
-        expected_output += "Leaving the Item Shop.\n"
-        self.assertEqual(mock_stdout.getvalue(), expected_output)
-        self.assertIn("Health Potion", game.player.inventory)
+    def test_visit_item_shop_buy_item(self):
+        with patch("builtins.input", return_value="Health Potion"):
+            self.game_instance.create_player()
+            self.game_instance.visit_item_shop()
+            self.assertIn("TestPlayer bought Health Potion", self.game_instance.player.inventory)
 
-    def test_game_display_player_stats(self):
-        game = Game()
-        game.player = Player(name="Player1", level=3, health=70, attack=18, defense=7, inventory=["Health Potion"])
-        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
-            game.display_player_stats()
-        expected_output = "\nPlayer Stats for Player1:\nLevel: 3\nHealth: 70\nAttack: 18\nDefense: 7\n"
-        expected_output += "Inventory: ['Health Potion']\n"
-        self.assertEqual(mock_stdout.getvalue(), expected_output)
+    def test_explore_encounter_enemy(self):
+        with patch("sys.stdout", new_callable=StringIO) as mock_stdout:
+            with patch("random.choice", return_value=self.enemy):
+                self.game_instance.create_player()
+                self.game_instance.explore()
+                output = mock_stdout.getvalue().strip()
+                self.assertIn("A wild TestEnemy appears!", output)
 
-     
-    
+    def test_game_loop_explore_option(self):
+        with patch("builtins.input", return_value="1"), patch("sys.stdout", new_callable=StringIO) as mock_stdout:
+            self.game_instance.create_player()
+            self.game_instance.game_loop()
+            output = mock_stdout.getvalue().strip()
+            self.assertIn("is exploring", output)
 
-if __name__ == '__main__':
+    def test_game_loop_visit_item_shop_option(self):
+        with patch("builtins.input", return_value="2"), patch("sys.stdout", new_callable=StringIO) as mock_stdout:
+            self.game_instance.create_player()
+            self.game_instance.game_loop()
+            output = mock_stdout.getvalue().strip()
+            self.assertIn("Welcome to the Item Shop!", output)
+
+    def test_game_loop_display_player_stats_option(self):
+        with patch("builtins.input", return_value="3"), patch("sys.stdout", new_callable=StringIO) as mock_stdout:
+            self.game_instance.create_player()
+            self.game_instance.game_loop()
+            output = mock_stdout.getvalue().strip()
+            self.assertIn("Player Stats for TestPlayer", output)
+
+    def test_game_loop_quit_option(self):
+        with patch("builtins.input", return_value="4"), patch("sys.stdout", new_callable=StringIO) as mock_stdout:
+            self.game_instance.create_player()
+            self.game_instance.game_loop()
+            output = mock_stdout.getvalue().strip()
+            self.assertIn("Thanks for playing! Goodbye.", output)
+
+if __name__ == "__main__":
     unittest.main()
