b634b336cb64efad9a236fb8c75f08df16fc8bdb
sai-krishna-eluru <34314057+sai-krishna-eluru@users.noreply.github.com>
2023-12-10 13:43:45 -0700

    Update testcode.py

--- testcode.py
+++ testcode.py
@@ -1,120 +1,106 @@
-import unittest
-import io
-import os
-from code import Task, TaskManager, TaskStatistics
+import coverage
+import subprocess
 
-class TestTaskManager(unittest.TestCase):
-    def setUp(self):
-        self.manager = TaskManager()
+cov = coverage.Coverage()
 
-    def test_add_task(self):
-        task = Task("Test task", 1)
-        self.manager.add_task(task)
-        self.assertEqual(len(self.manager.tasks), 1)
+class Animal:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        self.energy = 50
 
-    def test_get_priority_tasks(self):
-        task1 = Task("High priority", 3)
-        task2 = Task("Low priority", 1)
-        self.manager.add_task(task1)
-        self.manager.add_task(task2)
-        priority_1_tasks = self.manager.get_priority_tasks(1)
-        self.assertEqual(len(priority_1_tasks), 1)
-        self.assertEqual(priority_1_tasks[0].description, "Low priority")
+    def make_sound(self):
+        raise NotImplementedError("Subclasses must implement this method")
 
-    def test_mark_task_as_completed(self):
-        task = Task("Incomplete task", 2)
-        self.manager.add_task(task)
-        self.manager.mark_task_as_completed(0)
-        self.assertTrue(self.manager.tasks[0].completed)
+    def eat(self, food_energy):
+        self.energy += food_energy
 
-    def test_search_tasks(self):
-        task1 = Task("Read a book", 2)
-        task2 = Task("Write a book", 1)
-        self.manager.add_task(task1)
-        self.manager.add_task(task2)
-        matching_tasks = self.manager.search_tasks("book")
-        self.assertEqual(len(matching_tasks), 2)
-        self.assertEqual(matching_tasks[0].priority, 2)
-        self.assertEqual(matching_tasks[1].priority, 1)
+    def sleep(self, hours):
+        self.energy += 5 * hours
 
-    def test_save_and_load_tasks(self):
-        task1 = Task("Task 1", 1)
-        task2 = Task("Task 2", 2)
-        self.manager.add_task(task1)
-        self.manager.add_task(task2)
+class Dog(Animal):
+    def make_sound(self):
+        return "Woof!"
 
-        self.manager.save_tasks("test_tasks.pkl")
+class Cat(Animal):
+    def make_sound(self):
+        return "Meow!"
 
-        loaded_manager = TaskManager()
-        loaded_manager.load_tasks("test_tasks.pkl")
+class Bird(Animal):
+    def make_sound(self):
+        return "Tweet!"
 
-        self.assertEqual(len(loaded_manager.tasks), 2)
-        self.assertEqual(loaded_manager.tasks[0].description, "Task 1")
-        self.assertEqual(loaded_manager.tasks[1].priority, 2)
+class Zoo:
+    def __init__(self):
+        self.animals = []
 
-        os.remove("test_tasks.pkl")
+    def add_animal(self, animal):
+        self.animals.append(animal)
 
-    def test_remove_completed_tasks(self):
-        task1 = Task("Completed task", 1)
-        task1.completed = True
-        task2 = Task("Incomplete task", 2)
-        self.manager.add_task(task1)
-        self.manager.add_task(task2)
+    def feed_all_animals(self, food_energy):
+        for animal in self.animals:
+            animal.eat(food_energy)
 
-        self.assertEqual(len(self.manager.tasks), 2)
-        self.manager.remove_completed_tasks()
-        self.assertEqual(len(self.manager.tasks), 1)
-        self.assertEqual(self.manager.tasks[0].description, "Incomplete task")
+# Test Cases
+def test_animal_creation():
+    dog = Dog("Buddy", "Dog")
+    assert dog.name == "Buddy"
+    assert dog.species == "Dog"
+    assert dog.energy == 50
 
-    def test_display_tasks(self):
-        task1 = Task("Display task", 2)
-        self.manager.add_task(task1)
+def test_animal_sound():
+    cat = Cat("Whiskers", "Cat")
+    assert cat.make_sound() == "Meow!"
 
-        expected_output = "1. Priority: 2, Description: Display task, Status: Pending\n"
+def test_animal_actions():
+    dog = Dog("Max", "Dog")
+    dog.eat(20)
+    assert dog.energy == 70
 
-        with io.StringIO() as captured_output:
-            with contextlib.redirect_stdout(captured_output):
-                self.manager.display_tasks()
+    cat = Cat("Fluffy", "Cat")
+    cat.sleep(3)
+    assert cat.energy == 65
 
-            self.assertEqual(captured_output.getvalue(), expected_output)
+def test_bird_creation():
+    bird = Bird("Sparrow", "Bird")
+    assert bird.name == "Sparrow"
+    assert bird.species == "Bird"
+    assert bird.energy == 50
 
-    def test_get_high_priority_tasks(self):
-        task1 = Task("High priority", 3)
-        task2 = Task("Low priority", 1)
-        task3 = Task("Another high priority", 3)
-        self.manager.add_task(task1)
-        self.manager.add_task(task2)
-        self.manager.add_task(task3)
+def test_zoo():
+    zoo = Zoo()
 
-        high_priority_tasks = self.manager.get_high_priority_tasks()
-        self.assertEqual(len(high_priority_tasks), 2)
-        self.assertEqual(high_priority_tasks[0].description, "High priority")
-        self.assertEqual(high_priority_tasks[1].description, "Another high priority")
+    dog = Dog("Rex", "Dog")
+    cat = Cat("Whiskers", "Cat")
+    bird = Bird("Robin", "Bird")
 
-    def test_update_task_description(self):
-        task = Task("Old description", 1)
-        self.manager.add_task(task)
-        self.manager.update_task_description(0, "New description")
-        self.assertEqual(self.manager.tasks[0].description, "New description")
+    zoo.add_animal(dog)
+    zoo.add_animal(cat)
+    zoo.add_animal(bird)
 
-class TestTaskStatistics(unittest.TestCase):
-    def test_task_statistics(self):
-        manager = TaskManager()
-        task_stats = TaskStatistics(manager)
+    zoo.feed_all_animals(10)
 
-        task1 = Task("Task 1", 1)
-        task2 = Task("Task 2", 2)
-        task3 = Task("Task 3", 3)
-        manager.add_task(task1)
-        manager.add_task(task2)
-        manager.add_task(task3)
-
-        self.assertEqual(task_stats.count_tasks(), 3)
-
-        task1.completed = True
-        task2.completed = True
-        self.assertEqual(task_stats.count_completed_tasks(), 2)
-        self.assertEqual(task_stats.count_pending_tasks(), 1)
+    assert dog.energy == 60
+    assert cat.energy == 60
+    assert bird.energy == 60
 
 if __name__ == "__main__":
-    unittest.main()
+    cov.start()
+    # Run test cases
+    test_animal_creation()
+    test_animal_sound()
+    test_animal_actions()
+    test_bird_creation()
+    test_zoo()
+    cov.stop()
+
+    # Save coverage data
+    cov.save()
+
+    # Use the coverage run command to execute the script and generate the report
+    coverage_command = f"coverage run {__file__}"
+    subprocess.run(coverage_command, shell=True)
+
+    # Generate coverage report
+    cov.report(omit=[__file__], show_missing=False)
+    cov.html_report(omit=[__file__])
