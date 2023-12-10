3d06e7f2aa5fca17f7f01195b723fbcd2c91c1af
sai-krishna-eluru <34314057+sai-krishna-eluru@users.noreply.github.com>
2023-12-10 01:30:06 -0700

    Update code.py

--- code.py
+++ code.py
@@ -22,9 +22,6 @@ class TaskManager:
     def mark_task_as_completed(self, task_index):
         if 0 <= task_index < len(self.tasks):
             self.tasks[task_index].completed = True
-            print(f"Task '{self.tasks[task_index].description}' marked as completed.")
-        else:
-            print("Invalid task index.")
 
     def search_tasks(self, keyword):
         return [task for task in self.tasks if keyword.lower() in task.description.lower()]
@@ -47,6 +44,30 @@ class TaskManager:
         except Exception as e:
             print(f"An error occurred while loading tasks: {e}")
 
+    def remove_completed_tasks(self):
+        self.tasks = [task for task in self.tasks if not task.completed]
+
+    def get_high_priority_tasks(self):
+        return [task for task in self.tasks if task.priority == 3]
+
+    def update_task_description(self, task_index, new_description):
+        if 0 <= task_index < len(self.tasks):
+            self.tasks[task_index].description = new_description
+
+# Additional functionality
+class TaskStatistics:
+    def __init__(self, task_manager):
+        self.task_manager = task_manager
+
+    def count_tasks(self):
+        return len(self.task_manager.tasks)
+
+    def count_completed_tasks(self):
+        return len(self.task_manager.get_completed_tasks())
+
+    def count_pending_tasks(self):
+        return self.count_tasks() - self.count_completed_tasks()
+
 # Example usage
 if __name__ == "__main__":
     manager = TaskManager()
@@ -59,44 +80,25 @@ if __name__ == "__main__":
     manager.add_task(task2)
     manager.add_task(task3)
 
-    while True:
-        print("\nTask Manager Menu:")
-        print("1. Display Tasks")
-        print("2. Add Task")
-        print("3. Mark Task as Completed")
-        print("4. Search Tasks")
-        print("5. Save Tasks")
-        print("6. Load Tasks")
-        print("0. Exit")
+    manager.display_tasks()
 
-        choice = input("Enter your choice: ")
+    priority_2_tasks = manager.get_priority_tasks(2)
+    print("\nPriority 2 tasks:")
+    for task in priority_2_tasks:
+        print(f"Description: {task.description}")
 
-        if choice == "1":
-            manager.display_tasks()
-        elif choice == "2":
-            description = input("Enter task description: ")
-            priority = int(input("Enter task priority: "))
-            new_task = Task(description, priority)
-            manager.add_task(new_task)
-            print("Task added successfully.")
-        elif choice == "3":
-            task_index = int(input("Enter the index of the task to mark as completed: "))
-            manager.mark_task_as_completed(task_index - 1)
-        elif choice == "4":
-            keyword = input("Enter a keyword to search for tasks: ")
-            matching_tasks = manager.search_tasks(keyword)
-            print("\nMatching Tasks:")
-            for task in matching_tasks:
-                print(f"Priority: {task.priority}, Description: {task.description}")
-        elif choice == "5":
-            filename = input("Enter the filename to save tasks: ")
-            manager.save_tasks(filename)
-            print("Tasks saved successfully.")
-        elif choice == "6":
-            filename = input("Enter the filename to load tasks from: ")
-            manager.load_tasks(filename)
-            print("Tasks loaded successfully.")
-        elif choice == "0":
-            break
-        else:
-            print("Invalid choice. Please try again.")
+    manager.save_tasks("tasks.pkl")
+    manager.load_tasks("tasks.pkl")
+
+    print("\nAfter loading tasks:")
+    manager.display_tasks()
+
+    manager.remove_completed_tasks()
+    print("\nAfter removing completed tasks:")
+    manager.display_tasks()
+
+    task_stats = TaskStatistics(manager)
+    print("\nTask Statistics:")
+    print(f"Total tasks: {task_stats.count_tasks()}")
+    print(f"Completed tasks: {task_stats.count_completed_tasks()}")
+    print(f"Pending tasks: {task_stats.count_pending_tasks()}")
